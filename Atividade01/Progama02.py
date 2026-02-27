a9646791d4d9290c0af46d1baafffd9595231bce
jhionatanfreitas-code <jhionatan_freitas@estudante.sesisenai.org.br>
2026-02-27 19:43:56 +0000

    update

---
+++ Atividade01/Progama02.py
@@ -0,0 +1,4 @@
+nome = input("Insira o seu nome: ")
+numero1 = input("Insira o primeiro número: ")
+numero2 = input("Insira o segundo número: ")
+print("O usuário ",nome,"Obteve a soma dos dois valores de :",numero1 + numero2)
---
+++ Atividade01/Programa04.py
@@ -0,0 +1,12 @@
+nome = input("Nome do aluno: ")
+nota01 = float(input("Primeira nota: "))
+nota02 = float(input("Segunda nota: "))
+media = (nota01 + nota02) /2
+print("Média do aluno: ", media)
+if media >= 7:
+    print("O aluno está aprovado: ")
+else:print("O aluno está reprovado: ")
+ 
+
+    
+    
