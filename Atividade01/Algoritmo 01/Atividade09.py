568bca4e7df5eccf29d794b54974a84704ca2232
jhionatanfreitas-code <jhionatan_freitas@estudante.sesisenai.org.br>
2026-04-17 19:42:14 +0000

    update

--- Atividade01/Algoritmo 01/Atividade09.py
+++ Atividade01/Algoritmo 01/Atividade09.py
@@ -1,9 +1,7 @@
-pequeno = 10
-médio = 12
-grande = 15
+
 valor01 = int(input("Digite a quantidade de camisas pequenas: "))
 valor02 = int(input("Digite a quantidade de camisas médias: "))
 valor03 = int(input("Digite a quantidade de camisas grandes: "))
 
-total = valor01 * pequeno + valor02 * médio + valor03 * grande
+total = valor01 * 10 + valor02 * 12 + valor03 * 15
 print("O valor total arrecadado será de",total,"R$")
--- Atividade01/Algoritmo 01/atividade06.py
+++ Atividade01/Algoritmo 01/atividade06.py
@@ -1,4 +1,8 @@
-quilo = 12
-prato_peso = float(input("Insira o peso do prato: "))
-preço = quilo * prato_peso
-print("O valor a pagar será: ", preço)
+while True:
+    prato_peso = float(input("Insira o peso do prato em Kg: "))
+    if prato_peso >=0:
+        break
+    print("Valor inválido: ")
+
+preço = prato_peso * 12.0
+print("O valor a pagar será: ", preço, "Kg")
