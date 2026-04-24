7ca8630bddde30a8334fdd766ab56555e4958b3e
jhionatanfreitas-code <jhionatan_freitas@estudante.sesisenai.org.br>
2026-04-24 20:16:49 +0000

    update

---
+++ Atividade01/Lista de exercícios de algoritmos/Atividade17.py
@@ -0,0 +1,4 @@
+celsius = float(input("Digite a temperatura em graus Celsius: "))
+fahrenheit = (celsius * 1.8) + 32
+
+print(f"{celsius}°C equivalem a {fahrenheit:.1f}°F")
---
+++ Atividade01/Lista de exercícios de algoritmos/Atividade18.py
@@ -0,0 +1,7 @@
+horas_normais = float(input("Digite a quantidade de horas normais"))
+horas_extras = float(input("Digite a quantidade de horas extras"))
+
+valor_horas_normais = horas_normais * 10
+valor_horas_extras = horas_extras * 15
+
+print("O valor bruto é de: ")
