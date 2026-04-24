a7264510c79bad2ea650c550489d498ccda2d740
jhionatanfreitas-code <jhionatan_freitas@estudante.sesisenai.org.br>
2026-04-24 19:49:43 +0000

    update

---
+++ Atividade01/Lista de exercícios de algoritmos/Atividade14.py
@@ -0,0 +1,5 @@
+numero = float(input("Digite o diâmetro da pizza em cm: "))
+raio = numero / 2
+area = raio ** 2 * 3.14
+
+print("A area da pizza é de:", area)
---
+++ Atividade01/Lista de exercícios de algoritmos/Atividade15.py
@@ -0,0 +1,7 @@
+total = float(input("Valor total da conta: "))
+valor1 = total // 3
+usuário = total - (2* valor1)
+
+print ("O valor de carlos é: ", valor1)
+print ("O valor de andré é: ", valor1)
+print ("O valor de felipe é: ", valor1)
