58adab56085c7fdb05010c8a0e56b50d96a10776
jhionatanfreitas-code <jhionatan_freitas@estudante.sesisenai.org.br>
2026-05-08 19:00:08 +0000

    update

--- Atividade01/Lista de exercícios de algoritmos/Atividade34.py
+++ Atividade01/Lista de exercícios de algoritmos/Atividade34.py
@@ -2,4 +2,8 @@ salario = 1200
 c1 = 200
 c2 = 120
 
-tl_contas = (c1 * 1.02)
+tl_contas = (c1 * 1.02) + (c2 * 1.02)
+restante = salario - tl_contas
+
+print("Contas com multas: ", tl_contas)
+print("Restante do salário: ", restante)             
