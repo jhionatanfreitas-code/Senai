bd02ffd2a5f1abb9e98e3f6f6ae85a9445a7d7c0
jhionatanfreitas-code <jhionatan_freitas@estudante.sesisenai.org.br>
2026-05-08 17:34:16 +0000

    update

--- Atividade01/Lista de exercícios de algoritmos/Atividade20.py
+++ Atividade01/Lista de exercícios de algoritmos/Atividade20.py
@@ -2,4 +2,6 @@ quantidade_blusas = int(input("Digite a quantidade de blusas necessárias: "))
 metros_totais = quantidade_blusas * 120
 novelos = metros_totais // 125
 
-print (f"total de novelos necessários {novelos}")
+if metros_totais % 125 > 0:
+
+ print(f"total de novelos necessários {novelos}")
