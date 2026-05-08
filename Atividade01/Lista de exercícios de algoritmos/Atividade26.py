6d32c2b2c1f8774dfffbf592b79e3609c014bb30
jhionatanfreitas-code <jhionatan_freitas@estudante.sesisenai.org.br>
2026-05-08 17:52:47 +0000

    update

---
+++ Atividade01/Lista de exercícios de algoritmos/Atividade26.py
@@ -0,0 +1,7 @@
+numero01 = float(input("Digite o primeiro número para multiplicação: "))
+numero02 = float(input("Digite o segundo número para multiplicação: "))
+numero03 = float(input("Digite o terceiro número para multiplicação: "))
+
+algoritmo = numero01 * numero02 * numero03
+
+print("Resultado da multiplicação",algoritmo)
